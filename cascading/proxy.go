ed9e521747fddd868fd4cf90cc4f11b6b7ed9c9c
midbel <brun.nicolas21@gmail.com>
2019-11-29 13:17:20 +0100

    use one connection to cascade packet instead of a pool

--- cascading/proxy.go
+++ cascading/proxy.go
@@ -1,114 +1,169 @@
 package cascading
 
 import (
-	"bytes"
+	// "bytes"
 	"io"
 	"io/ioutil"
 	"net"
+	"sync"
 	"time"
 )
 
-const defaultSize = 4
-
 type proxy struct {
-	queue  chan net.Conn
-	buffer bytes.Buffer
+	net.Conn
 
-	addr string
+	mu     sync.Mutex
+	writer io.Writer
 }
 
-func Proxy(addr string, n int) (io.WriteCloser, error) {
+func Proxy(addr string, _ int) (io.WriteCloser, error) {
 	if _, _, err := net.SplitHostPort(addr); err != nil {
 		return nil, err
 	}
-	if n <= 0 {
-		n = defaultSize
-	}
-	p := proxy{
-		addr:  addr,
-		queue: make(chan net.Conn, n),
-	}
-	return &p, nil
-}
-
-func (p *proxy) Close() error {
-	err := p.flush()
-	for c := range p.queue {
-		c.Close()
-	}
-	return err
-}
-
-var syncword = []byte{0xf8, 0x2e, 0x35, 0x53}
-
-func (p *proxy) Write(bs []byte) (int, error) {
-	if len(bs) == 0 || (bytes.HasPrefix(bs, syncword) && p.buffer.Len() > 0) {
-		defer p.buffer.Reset()
-
-		c, err := p.pop()
-		if err != nil {
-			return 0, nil
-		}
-
-		xs, _ := ioutil.ReadAll(&p.buffer)
-		go func(c net.Conn, xs []byte) {
-			if _, err := c.Write(xs); err != nil {
-				c.Close()
-			} else {
-				p.push(c)
-			}
-		}(c, xs)
-	}
-
-	return p.buffer.Write(bs)
-}
-
-func (p *proxy) flush() error {
-	var err error
-	if p.buffer.Len() > 0 {
-		_, err = p.Write(nil)
-	}
-	return err
-}
-
-func (p *proxy) pop() (net.Conn, error) {
-	select {
-	case c := <-p.queue:
-		return c, nil
-	default:
-		return client(p.addr)
-	}
-}
-
-func (p *proxy) push(c net.Conn) {
-	select {
-	case p.queue <- c:
-	default:
-		c.Close()
-	}
-}
-
-func client(addr string) (net.Conn, error) {
-	// c, err := net.Dial("tcp", addr)
-	c, err := net.DialTimeout("tcp", addr, time.Second)
+	x, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
-	n := conn{
-		Conn:   c,
-		writer: c,
-		addr:   addr,
+	c := proxy{
+		Conn:   x,
+		writer: x,
 	}
-	return &n, nil
+	return &c, nil
 }
 
-type conn struct {
-	net.Conn
-	writer io.Writer
+func (c *proxy) Write(bs []byte) (int, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-	addr string
+	_, err := c.writer.Write(bs)
+	if err != nil {
+		switch c.Conn.(type) {
+		case *net.UDPConn:
+		case *net.TCPConn:
+			c.writer = ioutil.Discard
+
+			go c.reconnect()
+		}
+	}
+	return len(bs), nil
 }
 
-func (c *conn) Write(bs []byte) (int, error) {
-	return c.writer.Write(bs)
+func (c *proxy) reconnect() {
+	addr := c.RemoteAddr().String()
+	defer c.Conn.Close()
+	for {
+		x, err := net.DialTimeout("tcp", addr, time.Second*5)
+		if err == nil {
+			c.mu.Lock()
+			defer c.mu.Unlock()
+
+			c.writer, c.Conn = x, x
+			break
+		}
+	}
 }
+
+// const defaultSize = 4
+//
+// type proxy struct {
+// 	queue  chan net.Conn
+// 	buffer bytes.Buffer
+//
+// 	addr string
+// }
+//
+// func Proxy(addr string, n int) (io.WriteCloser, error) {
+// 	if _, _, err := net.SplitHostPort(addr); err != nil {
+// 		return nil, err
+// 	}
+// 	if n <= 0 {
+// 		n = defaultSize
+// 	}
+// 	p := proxy{
+// 		addr:  addr,
+// 		queue: make(chan net.Conn, n),
+// 	}
+// 	return &p, nil
+// }
+//
+// func (p *proxy) Close() error {
+// 	err := p.flush()
+// 	for c := range p.queue {
+// 		c.Close()
+// 	}
+// 	return err
+// }
+//
+// var syncword = []byte{0xf8, 0x2e, 0x35, 0x53}
+//
+// func (p *proxy) Write(bs []byte) (int, error) {
+// 	if len(bs) == 0 || (bytes.HasPrefix(bs, syncword) && p.buffer.Len() > 0) {
+// 		defer p.buffer.Reset()
+//
+// 		c, err := p.pop()
+// 		if err != nil {
+// 			return 0, nil
+// 		}
+//
+// 		xs, _ := ioutil.ReadAll(&p.buffer)
+// 		go func(c net.Conn, xs []byte) {
+// 			if _, err := c.Write(xs); err != nil {
+// 				c.Close()
+// 			} else {
+// 				p.push(c)
+// 			}
+// 		}(c, xs)
+// 	}
+//
+// 	return p.buffer.Write(bs)
+// }
+//
+// func (p *proxy) flush() error {
+// 	var err error
+// 	if p.buffer.Len() > 0 {
+// 		_, err = p.Write(nil)
+// 	}
+// 	return err
+// }
+//
+// func (p *proxy) pop() (net.Conn, error) {
+// 	select {
+// 	case c := <-p.queue:
+// 		return c, nil
+// 	default:
+// 		return client(p.addr)
+// 	}
+// }
+//
+// func (p *proxy) push(c net.proxy) {
+// 	select {
+// 	case p.queue <- c:
+// 	default:
+// 		c.Close()
+// 	}
+// }
+//
+// func client(addr string) (net.Conn, error) {
+// 	// c, err := net.Dial("tcp", addr)
+// 	c, err := net.DialTimeout("tcp", addr, time.Second)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	n := conn{
+// 		Conn:   c,
+// 		writer: c,
+// 		addr:   addr,
+// 	}
+// 	return &n, nil
+// }
+//
+// type conn struct {
+// 	net.Conn
+// 	writer io.Writer
+//
+// 	addr string
+// }
+//
+// func (c *proxy) Write(bs []byte) (int, error) {
+// 	return c.writer.Write(bs)
+// }
--- plugins/svs/svs.go
+++ plugins/svs/svs.go
@@ -21,24 +21,32 @@ import (
 
 const origin = "90"
 
+type UPI [32]byte
+
+func (u UPI) MarshalXML(e *xml.Encoder, _ xml.StartElement) error {
+	xs := u[:]
+	xs = bytes.Trim(xs, "\x00")
+	return e.EncodeElement(string(xs), xml.StartElement{Name: xml.Name{Local: "upi"}})
+}
+
 type metadata struct {
-	Property    uint8     `xml:"-"`
-	Source      uint8     `xml:"originator-id"`
-	Sequence    uint32    `xml:"originator-seq-no"`
-	Acquisition time.Time `xml:"acquisition-time"`
-	Auxiliary   time.Time `xml:"auxiliary-time"`
-	X           uint16    `xml:"source-x-size"`
-	Y           uint16    `xml:"source-y-size"`
-	Format      uint8     `xml:"format"`
-	Drop        uint16    `xml:"fdrp"`
-	OffsetX     uint16    `xml:"roi-x-offset"`
-	SizeX       uint16    `xml:"roi-x-size"`
-	OffsetY     uint16    `xml:"roi-y-offset"`
-	SizeY       uint16    `xml:"roi-y-size"`
-	ScaleX      uint16    `xml:"scale-x-size"`
-	ScaleY      uint16    `xml:"scale-y-size"`
-	Ratio       uint8     `xml:"scale-far"`
-	UPI         [32]byte  `xml:"-"`
+	Magic       uint8         `xml:"-"`
+	Acquisition time.Duration `xml:"acquisition-time"`
+	Sequence    uint32        `xml:"originator-seq-no"`
+	Auxiliary   time.Duration `xml:"auxiliary-time"`
+	Source      uint8         `xml:"originator-id"`
+	X           uint16        `xml:"source-x-size"`
+	Y           uint16        `xml:"source-y-size"`
+	Format      uint8         `xml:"format"`
+	Drop        uint16        `xml:"fdrp"`
+	OffsetX     uint16        `xml:"roi-x-offset"`
+	SizeX       uint16        `xml:"roi-x-size"`
+	OffsetY     uint16        `xml:"roi-y-offset"`
+	SizeY       uint16        `xml:"roi-y-size"`
+	ScaleX      uint16        `xml:"scale-x-size"`
+	ScaleY      uint16        `xml:"scale-y-size"`
+	Ratio       uint8         `xml:"scale-far"`
+	UPI         UPI           `xml:"user-packet-info"`
 }
 
 type converter struct {
@@ -87,7 +95,7 @@ func (c *converter) Process(i uint8, p panda.HRPacket) error {
 }
 
 func (c *converter) processData(file string, rs *bytes.Reader) error {
-	w, err := os.Create(file+".csv")
+	w, err := os.Create(file + ".csv")
 	if err != nil {
 		return err
 	}
@@ -96,12 +104,12 @@ func (c *converter) processData(file string, rs *bytes.Reader) error {
 	ws := csv.NewWriter(w)
 
 	b, err := rs.ReadByte()
-  if err != nil {
-    return err
-  }
-  vs := make([]string, int(b)+1)
-  vs[0] = "t"
-  for j := 0; j < int(b); j++ {
+	if err != nil {
+		return err
+	}
+	vs := make([]string, int(b)+1)
+	vs[0] = "t"
+	for j := 0; j < int(b); j++ {
 		var v uint16
 		if err := binary.Read(rs, binary.LittleEndian, &v); err != nil {
 			return err
@@ -109,7 +117,7 @@ func (c *converter) processData(file string, rs *bytes.Reader) error {
 		vs[j+1] = fmt.Sprintf("g2(t, %d)", v)
 	}
 	ws.Write(vs)
-  for i := 0; rs.Len() > 0; i++ {
+	for i := 0; rs.Len() > 0; i++ {
 		vs[0] = strconv.Itoa(i)
 		for j := 0; j < int(b); j++ {
 			var v float32
@@ -121,23 +129,23 @@ func (c *converter) processData(file string, rs *bytes.Reader) error {
 		ws.Write(vs)
 	}
 
-  ws.Flush()
-  return ws.Error()
+	ws.Flush()
+	return ws.Error()
 }
 
 func (c *converter) processMeta(file string, r io.Reader) error {
 	var info metadata
-  if err := binary.Read(r, binary.LittleEndian, &info); err != nil {
-    return err
-  }
-  w, err := os.Create(file+".xml")
-  if err != nil {
-    return err
-  }
-  defer w.Close()
+	if err := binary.Read(r, binary.LittleEndian, &info); err != nil {
+		return err
+	}
+	w, err := os.Create(file + ".xml")
+	if err != nil {
+		return err
+	}
+	defer w.Close()
 
-  e := xml.NewEncoder(w)
-  e.Indent("", "\t")
+	e := xml.NewEncoder(w)
+	e.Indent("", "\t")
 	return e.Encode(info)
 }
 
